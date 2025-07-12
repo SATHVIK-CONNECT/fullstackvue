a286ac228098a2f6d8ca09d6fa9fafd1729786e2
SATHVIK-CONNECT <sathvikpalivela0@gmail.com>
2025-07-12 12:52:35 +0530

    Updated

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -15,6 +15,7 @@ async function start() {
   app.use("/images", express.static(path.join(__dirname, "../assets")));
 
   app.use(express.static(path.resolve(__dirname, "../dist"), { maxAge: "1y", etag: false }));
+  // console.log("Static path:", path.resolve(__dirname, "../dist"));
 
   app.get("/api/products", async (req, res) => {
     const products = await db.collection("products").find({}).toArray();
@@ -73,11 +74,11 @@ async function start() {
     res.json(populatedCart);
   });
 
-  app.get("*", (req, res) => {
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
   });
 
-  const port = process.env.PORT || 8000;
+  const port = 8000;
 
   app.listen(8000, () => {
     console.log("Running on port: " + port);
