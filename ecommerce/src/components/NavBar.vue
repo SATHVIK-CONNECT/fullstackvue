9685b414382be83c623eb0c274aa217d69afd6b3
SATHVIK-CONNECT <sathvikpalivela0@gmail.com>
2025-07-12 13:10:11 +0530

    Updated

--- ecommerce/src/components/NavBar.vue
+++ ecommerce/src/components/NavBar.vue
@@ -12,9 +12,6 @@
       <router-link to="/cart" class="cart-link">
         <button style="background-color: transparent; color: black">Shopping Cart</button>
       </router-link>
-      <button v-if="!user" @click="signIn" style="background-color: transparent; color: black">
-        Sign in
-      </button>
       <button v-if="user" @click="signOut" style="background-color: transparent; color: black">
         Sign out
       </button>
@@ -22,14 +19,7 @@
   </div>
 </template>
 <script>
-// import { getAuth, signOut } from "firebase/auth";
-import {
-  getAuth,
-  signOut,
-  sendSignInLinkToEmail,
-  isSignInWithEmailLink,
-  signInWithEmailLink,
-} from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 import vue from "../assets/logo.png";
 export default {
   name: "NavBar",
@@ -44,26 +34,6 @@ export default {
       const auth = getAuth();
       signOut(auth);
     },
-    async signIn() {
-      const email = prompt("Please enter the email:");
-      const auth = getAuth();
-      const actionCodeSettings = {
-        url: `https://fullstackvue.onrender.com/products/${this.$route.params.productId}`,
-        handleCodeInApp: true,
-      };
-      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-      alert("Check your Email");
-      window.localStorage.setItem("emailForSignIn", email);
-    },
-  },
-  async created() {
-    const auth = getAuth();
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      const email = window.localStorage.getItem("emailForSignIn");
-      await signInWithEmailLink(auth, email, window.location.href);
-      alert("Successfully logged in ");
-      window.localStorage.removeItem("emailForSignIn");
-    }
   },
 };
 </script>
